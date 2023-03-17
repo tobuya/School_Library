d5cc7d441a28b396c8dacef79dc8faee3d2372a3
tobuya <thomasobuya95@gmail.com>
2023-03-17 22:40:48 +0300

    Add name and age parameters

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(classroom)
-    super
+  def initialize(name, age, classroom)
+    super(name, age)
     @classroom = classroom
   end
 
